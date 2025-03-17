3a85ae54d27bd969a05b8fc93e45dd30ed29edac
celisawalker <celisa.walker@gmail.com>
2025-03-17 15:36:54 -0700

    wip: defined books/index.ejs route, created books/new.ejs form

--- controllers/books.js
+++ controllers/books.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-const User = require('../models/user.js');
+router.get("/", async (req, res) => {
+    try{
+        const currentUser = await User.findById(req.session.user._id)
 
+        res.render("books/index.ejs", {books: currentUser.books});
+        //we are now going to pass the current user's pantry items to the index page
+    } catch (error){
+        console.log(error);
+        res.redirect("/");
+    }
+})
 
+router.get("/new", async (req, res) => {
+    // res.render("books/new.ejs")
+    res.render("books/new.ejs");
+})
 
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,5 +1,50 @@
 const mongoose = require('mongoose');
 
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true,
+    enum: [
+      "fiction",
+      "non-fiction",
+      "romance",
+      "fantasy",
+      "science-fiction",
+      "mystery",
+      "coming-of-age",
+      "young-adult",
+      "new-adult",
+      "self-help",
+      "poetry",
+      "history"
+    ]
+  },
+  currentlyReading: {
+    type: Boolean
+  },
+
+  isFinished: {
+    type: Boolean
+  },
+
+  dnf: {
+    type: Boolean
+  },
+
+  rating: {
+    type: String,
+    required: true
+  },
+  
+  review: {
+    type: String,
+  }
+})
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +54,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  books: [bookSchema]
 });
 
 const User = mongoose.model('User', userSchema);
